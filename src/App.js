4a0d792834b7ee277c453c518136225db18325b9
SyedYaseenUddin <yaseenuddin.qadri@gmail.com>
2022-01-21 11:17:53 +0530

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
+import { Switch, Route } from 'react-router-dom';
+import About from "./About"
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+
+
+      <Switch>
+          <Route exact path="/" component={About}></Route>
+      </Switch>
     </div>
+    
   );
 }
